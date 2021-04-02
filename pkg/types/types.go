3901d3465ba021476401304a9883e638aa5c4479
Sunatullo <gafurovsunatullo@gmail.com>
2021-04-02 15:46:36 +0500

    add status types

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -6,9 +6,20 @@ type Money int64
 // Category describes payment category
 type Category string
 
+//Status info about payment status
+type Status string
+
+//Statuses for payments
+const (
+	StatusOk         Status = "OK"
+	StatusFail       Status = "FAIL"
+	StatusInProgress Status = "INPROGRESS"
+)
+
 // Payment describes structure for payments
 type Payment struct {
 	ID       int
 	Amount   Money
 	Category Category
+	Status   Status
 }
